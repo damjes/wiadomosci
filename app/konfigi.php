957ff2ee9a49361539ba4825029db378cf888768
Damjes <git@damj.es>
2020-03-29 22:15:21 +0200

    Usunięto debugowanie

--- app/konfigi.php
+++ app/konfigi.php
@@ -1,9 +1,7 @@
 <?php
 
 function czytajKonfigi($f3) {
-	echo '<p>Czytam konfigi... ';
 	$f3->config('app/sciezki.ini');
 	$f3->config('konfig.ini');
-	echo 'OK</p>';
 }
 
--- app/model/maper.php
+++ app/model/maper.php
@@ -1,14 +1,11 @@
 <?php
 
 function utworzPolaczenie($f3) {
-	echo '<p>Ładuję połączenie... ';
 	$dsn = $f3->get('db.dsn');
-	echo $dsn;
 	$user = $f3->get('db.user');
 	$haslo = $f3->get('db.haslo');
 
 	$baza = new \DB\SQL($dsn, $user, $haslo);
 	$f3->set('db.polaczenie', $baza);
-	echo 'OK</p>';
 }
 
